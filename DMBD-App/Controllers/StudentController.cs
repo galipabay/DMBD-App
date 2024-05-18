3835ce27516072100fc3a15fa6ff312d5f9d5d56
Galip Abay <galipabay37@gmail.com>
2024-05-18 16:04:41 +0300

    18.05.2024 (GALIPA) controlerlar duzeltildi buglar eritildi api service yazildi

--- DMBD-App/Controllers/StudentController.cs
+++ DMBD-App/Controllers/StudentController.cs
@@ -7,96 +7,96 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DMBD_App.Controllers
 {
-    public class StudentController : Controller
-    {
-        private readonly IService<Student> _services;
+	public class StudentController : Controller
+	{
 
-        private readonly IMapper _mapper;
+		private readonly IService<Student> _services;
 
-
-        public StudentController(IService<Student> services,IMapper mapper)
-        {
-            _services = services;
-            _mapper = mapper;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            return View(await _services.GetAllAsync());
-        }
-
-        public async Task<IActionResult> Save()
-        {
-            var students = await _services.GetAllAsync();
-
-            var studentDto = _mapper.Map<List<StudentDto>>(students.ToList());
-
-            ViewBag.students = new SelectList(studentDto, "Id", "TcNo");
-
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Save(StudentDto studentDto)
-
-        {
-            if (ModelState.IsValid)
-            {
-                await _services.AddAsync(_mapper.Map<Student>(studentDto));
-                return RedirectToAction(nameof(Index));
-            }
-             
-            var students = await _services.GetAllAsync();
-
-            var studentsDto = _mapper.Map<List<StudentDto>>(students.ToList());
-
-            ViewBag.students = new SelectList(studentsDto, "Id", "Name");
-            return View();
-        }
-
-        [ServiceFilter(typeof(NotFoundFilter<Student>))]
-        public async Task<IActionResult> Update(int id)
-        {
-            var student = await _services.GetByIdAsync(id);
+		private readonly IMapper _mapper;
 
 
-            var students = await _services.GetAllAsync();
+		public StudentController(IService<Student> services,IMapper mapper)
+		{
+			_services = services;
+			_mapper = mapper;
+		}
 
-            var studentsDto = _mapper.Map<List<StudentDto>>(students.ToList());
+		public async Task<IActionResult> Index()
+		{			
+			return View("~/Views/Home/Index.cshtml");
+		}
 
-            ViewBag.students = new SelectList(studentsDto, "Id", "Name", student.StudentId);
+		public async Task<IActionResult> Save()
+		{
+			var students = await _services.GetAllAsync();
 
-            return View(_mapper.Map<StudentDto>(student));
+			var studentDto = _mapper.Map<List<StudentDto>>(students.ToList());
 
-        }
-        [HttpPost]
-        public async Task<IActionResult> Update(StudentDto studentDto)
-        {
-            if (ModelState.IsValid)
-            {
-                await _services.UpdateAsync(_mapper.Map<Student>(studentDto));
-                return RedirectToAction(nameof(Index));
-            }
+			ViewBag.students = new SelectList(studentDto, "Id", "TcNo");
 
-            var students = await _services.GetAllAsync();
+			return View(studentDto);
+		}
 
-            var studentsDto = _mapper.Map<List<StudentDto>>(students.ToList());
+		[HttpPost]
+		public async Task<IActionResult> Save(StudentDto studentDto)
+		{
+			if (ModelState.IsValid)
+			{
+				await _services.AddAsync(_mapper.Map<Student>(studentDto));
+				return RedirectToAction(nameof(Index));
+			}
+			 
+			var students = await _services.GetAllAsync();
 
-            ViewBag.students = new SelectList(studentsDto, "Id", "Name", studentDto.StudentId);
+			var studentsDto = _mapper.Map<List<StudentDto>>(students.ToList());
 
-            return View(studentDto);
+			ViewBag.students = new SelectList(studentsDto, "Id", "Name");
+			return View(studentDto);
+		}
 
-        }
+		[ServiceFilter(typeof(NotFoundFilter<Student>))]
+		public async Task<IActionResult> Update(int id)
+		{
+			var student = await _services.GetByIdAsync(id);
 
 
-        public async Task<IActionResult> Remove(int id)
-        {
-            var student = await _services.GetByIdAsync(id);
+			var students = await _services.GetAllAsync();
+
+			var studentsDto = _mapper.Map<List<StudentDto>>(students.ToList());
+
+			ViewBag.students = new SelectList(studentsDto, "Id", "Name", student.StudentId);
+
+			return View(_mapper.Map<StudentDto>(student));
+
+		}
+		[HttpPost]
+		public async Task<IActionResult> Update(StudentDto studentDto)
+		{
+			if (ModelState.IsValid)
+			{
+				await _services.UpdateAsync(_mapper.Map<Student>(studentDto));
+				return RedirectToAction(nameof(Index));
+			}
+
+			var students = await _services.GetAllAsync();
+
+			var studentsDto = _mapper.Map<List<StudentDto>>(students.ToList());
+
+			ViewBag.students = new SelectList(studentsDto, "Id", "Name", studentDto.StudentId);
+
+			return View(studentDto);
+
+		}
 
 
-            await _services.RemoveAsync(student);
+		public async Task<IActionResult> Remove(int id)
+		{
+			var student = await _services.GetByIdAsync(id);
 
-            return RedirectToAction(nameof(Index));
-        }
-    }
+
+			await _services.RemoveAsync(student);
+
+			return RedirectToAction(nameof(Index));
+		}
+	}
 }
---
+++ DMBD-App/Services/StudentApiService.cs
@@ -0,0 +1,50 @@
+﻿using DMBD.Kernel.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMBD_App.Services
+{
+	public class StudentApiService
+	{
+		
+			private readonly HttpClient _httpClient;
+
+			public StudentApiService(HttpClient httpClient)
+			{
+				_httpClient = httpClient;
+			}
+
+			public async Task<StudentDto> GetByIdAsync(int id)
+			{
+
+				var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<StudentDto>>($"products/{id}");
+				return response.Data;
+
+
+			}
+
+			public async Task<StudentDto> SaveAsync(StudentDto newStudent)
+			{
+				var response = await _httpClient.PostAsJsonAsync("products", newStudent);
+
+				if (!response.IsSuccessStatusCode) return null;
+
+				var responseBody = await response.Content.ReadFromJsonAsync<CustomResponseDto<StudentDto>>();
+
+				return responseBody.Data;
+
+			}
+			public async Task<bool> UpdateAsync(StudentDto newStudent)
+			{
+				var response = await _httpClient.PutAsJsonAsync("products", newStudent);
+
+				return response.IsSuccessStatusCode;
+			}
+			public async Task<bool> RemoveAsync(int id)
+			{
+				var response = await _httpClient.DeleteAsync($"products/{id}");
+
+				return response.IsSuccessStatusCode;
+			}
+
+	}
+}
