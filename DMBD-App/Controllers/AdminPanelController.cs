9089a24bf244ae34271e21420739afc97e4d91aa
ahsalmonskinroll <kumsal.celebiii@gmail.com>
2024-05-23 01:59:31 +0300

    insta pull

--- DMBD-App/Controllers/AdminPanelController.cs
+++ DMBD-App/Controllers/AdminPanelController.cs
@@ -9,64 +9,91 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DMBD_App.Controllers
 {
-	public class AdminPanelController : Controller
-	{
+    public class AdminPanelController : Controller
+    {
 
-		private readonly IService<AdminUser> _services;
-		private readonly IMapper _mapper;
-		private readonly AppDbContext _context;
+        private readonly IService<AdminUser> _services;
+        private readonly IMapper _mapper;
+        private readonly AppDbContext _context;
 
-		public AdminPanelController(IService<AdminUser> services, IMapper mapper, AppDbContext context)
-		{
-			_services = services;
-			_mapper = mapper;
-			_context = context;
-		}
+        public AdminPanelController(IService<AdminUser> services, IMapper mapper, AppDbContext context)
+        {
+            _services = services;
+            _mapper = mapper;
+            _context = context;
+        }
 
-		public async Task<IActionResult> AddAdmin()
-		{
-			return View();
-		}
+        public async Task<IActionResult> AddAdmin()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Save(AdminUser adminUser)
-		{
+        [HttpPost]
+        public async Task<IActionResult> Save(AdminUser adminUser)
+        {
 
-			if (ModelState.IsValid)
-			{
-				await _services.AddAsync(_mapper.Map<AdminUser>(adminUser));
-				return Redirect(nameof(Index));
-			}
-			return Redirect(nameof(Index));
-		}
+            if (ModelState.IsValid)
+            {
+                await _services.AddAsync(_mapper.Map<AdminUser>(adminUser));
+                return Redirect(nameof(Index));
+            }
+            return Redirect(nameof(Index));
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Login(AdminUser adminUser)
-		{
-			var user = await _services.AnyAsync(x => x.MailAddres == adminUser.MailAddres && x.Password == adminUser.Password );
-			 
-			if (user == false)
-			{
-				// Giriş başarısız, hata mesajı döndür
-				ModelState.AddModelError(string.Empty, "Geçersiz kullanıcı adı veya şifre.");
-				return View("AdminPanelLogin", adminUser);
-			}
+        [HttpPost]
+        public async Task<IActionResult> AdminList(AdminUser adminUser)
+        {
 
-			// Giriş başarılı, yönlendirme yap	
-			return RedirectToAction("AdminPanel","Home");
-		}
+            var subjects = _context.AdminUsers;
 
-		//public async Task<AdminUser> AuthenticateAsync(string username, string password)
-		//{
-		//	// Şifreyi hashleyip karşılaştırmak daha güvenli bir yaklaşım olur
-		//	var user = await _context.AdminUsers.SingleOrDefaultAsync(x => x.MailAddres == username && x.Password == password);
+            ViewBag.Subjects = new SelectList(subjects, "Id", "Name");
 
-		//	if (user == null)
-		//	{
-		//		return null;
-		//	}
+            if (ModelState.IsValid)
+            {
+                await _services.AddAsync(_mapper.Map<AdminUser>(adminUser));
+                return Redirect(nameof(Index));
+            }
+            return Redirect(nameof(Index));
+        }
 
-		//	return user;
-		//}
-	}
+        [HttpPost]
+        public async Task<IActionResult> Login(AdminUser adminUser)
+        {
+            var user = await _services.AnyAsync(x => x.MailAddres == adminUser.MailAddres && x.Password == adminUser.Password);
+
+            if (user == false)
+            {
+                // Giriş başarısız, hata mesajı döndür
+                ModelState.AddModelError(string.Empty, "Geçersiz kullanıcı adı veya şifre.");
+                return View("AdminPanelLogin", adminUser);
+            }
+
+            // Giriş başarılı, yönlendirme yap	
+            return RedirectToAction("AdminPanel", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var adminUser = await _services.GetByIdAsync(id);
+            if (adminUser != null)
+            {
+                await _services.RemoveAsync(adminUser);
+            }
+            return RedirectToAction(nameof(AdminList));
+        }
+
+        //public async Task<AdminUser> AuthenticateAsync(string username, string password)
+        //{
+        //	// Şifreyi hashleyip karşılaştırmak daha güvenli bir yaklaşım olur
+        //	var user = await _context.AdminUsers.SingleOrDefaultAsync(x => x.MailAddres == username && x.Password == password);
+
+        //	if (user == null)
+        //	{
+        //		return null;
+        //	}
+
+        //	return user;
+        //}
+    }
 }
